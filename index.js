0f7bbab7284a34db8a62cf1d66d80b8ec2f12e18
Clifford Okpai <meetmancliff@yahoo.com>
2021-03-27 19:56:10 -0500

    toogle functionality added

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+const toggleEl = document.querySelector('.toggle-container');
+const prices = document.querySelectorAll('.sub-price');
+const frequency = document.querySelectorAll('.sub-freq');
+let monthlyBronzePrice = 39.99;
+let annualBronzePrice = 479.88;
+
 const navSlide = () => {
   const Hamburger = document.querySelector('.hamburger');
   const nav = document.querySelector('.nav-links');
@@ -28,3 +34,30 @@ const currentDate = new Date().getFullYear();
 document.querySelector(
   ' footer'
 ).textContent = `©️ ${currentDate} - Clifford Okpai`;
+
+//add can event listener to the toggle element
+toggleEl.addEventListener('change', (e) => {
+  if (!e.target.checked) {
+    //monthly is selected
+    annualBronzePrice = 479.88; //reset the annual price
+    prices.forEach((el) => {
+      el.innerHTML = `$${monthlyBronzePrice.toFixed(2)}`;
+      monthlyBronzePrice += 20;
+    });
+
+    frequency.forEach((el) => {
+      el.innerHTML = `1 Delivery per Month`;
+    });
+  } else {
+    //annually is selected
+    monthlyBronzePrice = 39.99; //reset the monthly price
+    prices.forEach((el) => {
+      el.innerHTML = `$${annualBronzePrice.toFixed(2)}`;
+      annualBronzePrice += 240;
+    });
+
+    frequency.forEach((el) => {
+      el.innerHTML = `12 Deliveries per Year`;
+    });
+  }
+});
